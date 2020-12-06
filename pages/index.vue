5633e5ca0ed9e1738be4e3e4dcd2ca9c9d7d711b
ShugoYabuno <shugo.ybn@gmai.com>
2020-12-06 19:58:22 +0900

    ビデオ再生まで

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,39 @@
 <template>
-  <h1>テスト</h1>
+  <div class="container">
+    <input
+      ref="fileVideo"
+      type="file"
+      accept="video/*"
+      @change="handleChangeVideo"
+    />
+    <video-player :options="videoOptions" v-if="videoOptions" />
+  </div>
 </template>
 
 <script>
+import VideoPlayer from "~/components/Atoms/VideoPlayer.vue"
+import "video.js/dist/video-js.css"
+
 export default {
   data() {
     return {
       restaurants: [],
       videoInput: "",
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        sources: [
+          {
+            src:
+              "https://video-share-raw-data.s3.amazonaws.com/post/Lh5NGl7Msc6HucgbQ45x",
+            type: "video/mp4",
+          },
+        ],
+      },
     }
+  },
+  components: {
+    VideoPlayer,
   },
   async mounted() {
     // const firebaseParams = {
@@ -21,92 +46,47 @@ export default {
     // console.log(process.env.AWS_ACCESSKEYID)
     // console.log(process.env.AWS_SECRETACCESSKEY)
   },
-  // methods: {
-  //   async handleChangeVideo(e) {
+  methods: {
+    getFileVideo() {
+      const files = this.$refs.fileVideo
+      const fileVideo = files.files[0]
+
+      return fileVideo
+    },
+    async handleChangeVideo() {
+      console.log("テスト")
+      const fileVideo = this.getFileVideo()
+      console.log(fileVideo)
+
+      if (fileVideo.type.startsWith("video/")) this.videoS3Upload()
+    },
+    async videoS3Upload() {
+      const fileVideo = this.getFileVideo()
+
+      const response = await this.$store.dispatch("video/upload", { fileVideo })
+
+      console.log(response)
+    },
+  },
+  // async getThumbnailS3(_name) {
+  //   let getThumb = ""
+  //   if (this.videoInput) {
   //     const files = this.$refs.fileVideo
   //     const fileVideo = files.files[0]
-  //     // if (this.validateImageUploads(fileImg)) return
-
-  //     if (fileVideo.type.startsWith("video/")) {
-  //       this.videoS3Upload()
-  //     }
-  //   },
-  //   // async getThumbnailS3(_name) {
-  //   //   let getThumb = ""
-  //   //   if (this.videoInput) {
-  //   //     const files = this.$refs.fileVideo
-  //   //     const fileVideo = files.files[0]
-  //   //     const fileBase64 = await this.getBase64(fileVideo)
-  //   //     console.log(fileBase64)
-  //   //     getThumb = await this.videoS3Upload(_name, fileBase64)
-  //   //     console.log(getThumb)
-  //   //   }
-  //   //   return getThumb
-  //   // },
-  //   // getBase64(file) {
-  //   //   return new Promise((resolve, reject) => {
-  //   //     const reader = new FileReader()
-  //   //     reader.readAsDataURL(file)
-  //   //     reader.onload = () => resolve(reader.result)
-  //   //     reader.onerror = (error) => reject(error)
-  //   //   })
-  //   // },
-  //   async videoS3Upload() {
-  //     const files = this.$refs.fileVideo
-  //     const fileVideo = files.files[0]
-  //     const firestoreParams = {
-  //       col: "videos",
-  //       data: {
-  //         name: fileVideo.name,
-  //       },
-  //     }
-  //     const dbResponse = await this.$store.dispatch(
-  //       "db/create",
-  //       firestoreParams
-  //     )
-  //     const firestoreId = dbResponse.id
-
-  //     const fileBase64 = await new Promise((resolve, reject) => {
-  //       const reader = new FileReader()
-  //       reader.readAsDataURL(fileVideo)
-  //       reader.onload = () => resolve(reader.result)
-  //       reader.onerror = (error) => reject(error)
-  //     })
-
+  //     const fileBase64 = await this.getBase64(fileVideo)
   //     console.log(fileBase64)
-
-  //     const fileData = fileBase64.replace(/^data:\w+\/\w+;base64,/, "")
-  //     const fileName = `post/${firestoreId}`
-  //     const params = {
-  //       Bucket: process.env.S3_BUCKET_RAW_DATA,
-  //       Key: fileName,
-  //       ContentType: "video/mp4",
-  //       Body: Buffer.from(fileData, "base64"),
-  //       ACL: "public-read",
-  //       CacheControl: "no-cache, no-store",
-  //     }
-  //     const s3Url = `https://${process.env.S3_BUCKET_RAW_DATA}.s3.amazonaws.com/${fileName}`
-  //     const s3ResponseUrl = await new Promise((resolve) => {
-  //       s3.putObject(params, (err, data) => {
-  //         if (err) {
-  //           console.log("err")
-  //           resolve(err)
-  //         } else {
-  //           console.log("success")
-  //           resolve(s3Url)
-  //         }
-  //       })
-  //     })
-
-  //     console.log(s3ResponseUrl)
-
-  //     firestoreParams.id = firestoreId
-  //     firestoreParams.data.s3_url = s3ResponseUrl
-
-  //     const dbUpdate = await this.$store.dispatch("db/update", firestoreParams)
-  //     console.log(dbUpdate)
-  //     // return s3ResponseUrl
-  //   },
+  //     getThumb = await this.videoS3Upload(_name, fileBase64)
+  //     console.log(getThumb)
+  //   }
+  //   return getThumb
+  // },
+  // getBase64(file) {
+  //   return new Promise((resolve, reject) => {
+  //     const reader = new FileReader()
+  //     reader.readAsDataURL(file)
+  //     reader.onload = () => resolve(reader.result)
+  //     reader.onerror = (error) => reject(error)
+  //   })
   // },
 }
 </script>
