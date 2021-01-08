c1503fa22791f59d9fcd94f9cb8241393f421297
ShugoYabuno <shugo.ybn@gmai.com>
2021-01-09 00:49:28 +0900

    make it better

--- api/index.js
+++ api/index.js
@@ -1,47 +1,10 @@
-/**
- * Main Application File
- */
-"use strict"
+const express = require("express")
+const path = require("path")
+const PORT = process.env.PORT || 5000
 
-/**
- * Module dependencies.
- */
-import configure from "./lib/configure.js"
-import express from "express"
-import header from "./lib/header.js"
-import router from "./routes/v1"
-
-const app = express()
-
-/**
- * Configure Express Server.
- */
-configure(app)
-
-/**
- * header dependencies.
- */
-header(app)
-
-/**
- * API Routing.
- */
-app.use("/v1", router)
-
-/**
- * Test API.
- */
-app.get("/header", (req, res) => {
-  console.log(res.connection._httpMessage.socket._httpMessage)
-  res.json({
-    username: "test successful!",
-  })
-})
-
-/**
- * Start Server.
- */
-module.exports = {
-  path: "/api",
-  handler: app,
-}
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
