001cad512f26656a904ea57af59a52bc772fa9e2
ShugoYabuno <shugo.ybn@gmai.com>
2021-01-15 20:29:43 +0900

    add: ファイルアップロードプログレスバー実装

--- components/Atoms/VideoUploader.vue
+++ components/Atoms/VideoUploader.vue
@@ -2,7 +2,7 @@
   <div>
     <label
       for="uploader"
-      class="flex-ij-center w-10/12 rounded-full border-1-solid p-3 shadow bg-secondary border-secondary text-white">
+      class="flex-ij-center w-10/12 rounded-full p-3 shadow bg-secondary text-white">
       <font-awesome-icon
         icon="file-upload"
         class="fa-2x" />
@@ -16,6 +16,25 @@
       class="hidden"
       multiple
       @change="handleChangeVideos">
+    <div
+      v-if="uploadTasksProgress.length >= 1"
+      class="fixed w-80 bottom-10 right-10 shadow-lg rounded-sm overflow-hidden z-10 bg-gray-30">
+      <div class="bg-gray-100 text-gray-10 p-4">
+        <p>Uploading {{ uploadTasksProgress.length }} items</p>
+      </div>
+      <div
+        v-for="(task, index) in uploadTasksProgress"
+        :key="index"
+        class="p-2 border-solid border-t border-gray-50"
+        :class="{ 'border-none': index == 0 }">
+        <p>{{ task.fileName }}</p>
+        <progress
+          ref="progress"
+          class="w-full h-2 bg-lightgreen"
+          :value="`${task.progress}`"
+          max="100" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,6 +56,17 @@ export default {
   data() {
     return {
       videoInput: "",
+      uploadTasksProgress: []
+    }
+  },
+  computed: {
+    uploadTasks: function() {
+      return this.$store.getters["video/uploadTasks"]
+    }
+  },
+  watch: {
+    uploadTasks(_val) {
+      this.uploadTasksProgress = _val
     }
   },
   methods: {
--- store/video.js
+++ store/video.js
@@ -1,3 +1,4 @@
+import Vue from "vue"
 import { firebase, firestore } from "~/plugins/firebase"
 import firestoreService from "~/plugins/firestoreService"
 
@@ -14,7 +15,20 @@ const format2base64Data = async (_fileVideo) => {
   return fileBase64
 }
 
-export const state = () => {}
+export const state = () => ({
+  uploadTasks: []
+})
+
+export const mutations = {
+  updateUploadTasks (state, payload) {
+    const { index, task } = payload
+
+    Vue.set(state.uploadTasks, index, task)
+  },
+  removeUploadTasks (state, payload) {
+    state.uploadTasks = []
+  }
+}
 
 export const actions = {
   async upload(context, value) {
@@ -22,7 +36,8 @@ export const actions = {
 
     const uploadStatus = await new Promise((resolve, reject) => {
       let counter = 0
-      Object.keys(fileVideos).forEach(async (_key, index) => {
+
+      Object.keys(fileVideos).forEach(async (_key, _index) => {
         const file = fileVideos[_key]
         // console.log(file)
         const data = {
@@ -31,27 +46,62 @@ export const actions = {
         }
         const resServiceAdd = await firestoreService.add("videos", data)
 
-        console.log(resServiceAdd)
+        // console.log(resServiceAdd)
 
         const { documentId } = resServiceAdd
 
         const storageRef = firebase.storage().ref()
         const ref = storageRef.child(`videos/${documentId}`)
-        const successUpload = await ref.put(file).then(res => res.state === "success")
-        if(!successUpload) return
 
-        const videoUrl = await ref.getDownloadURL()
-        const contentType = await ref.getMetadata().then(metadata => metadata.contentType)
-        const resUpdate = await firestoreService.update("videos", documentId, {
-          storageUrl: videoUrl,
-          contentType
+        const uploadTask = ref.put(file)
+        console.log(uploadTask)
+        uploadTask.on("state_changed", function(snapshot){
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          // console.log("Upload is " + progress + "% done")
+
+          const payload = {
+            index: _index,
+            task: {
+              fileName: file.name,
+              progress
+            }
+          }
+          context.commit("updateUploadTasks", payload)
+
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              // console.log("Upload is paused")
+              break
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              // console.log("Upload is running")
+              break
+          }
+        }, function(error) {
+          // Handle unsuccessful uploads
+        }, async function() {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+
+          // uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+          //   console.log("File available at", downloadURL)
+          // })
+          const videoUrl = await ref.getDownloadURL()
+          const contentType = await ref.getMetadata().then(metadata => metadata.contentType)
+          const resUpdate = await firestoreService.update("videos", documentId, {
+            storageUrl: videoUrl,
+            contentType
+          })
+
+          counter += 1
+
+          if(counter === fileVideos.length) resolve("success")
         })
-
-        counter += 1
-
-        if(counter === fileVideos.length) resolve("success")
       })
     })
+
+    context.commit("removeUploadTasks")
 
     console.log(uploadStatus)
 
@@ -76,4 +126,10 @@ export const actions = {
 
     return videos
   }
+}
+
+export const getters = {
+  uploadTasks (state) {
+    return state.uploadTasks
+  },
 }
