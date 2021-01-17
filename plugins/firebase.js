376d710c9f0e9ac2cc8ac7b5fdf4f8d07a3690e5
ShugoYabuno <shugo.ybn@gmai.com>
2021-01-17 23:45:04 +0900

    firebase configを.envで管理

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,17 +2,10 @@ import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/firestore"
 
+const firebaseConfig = require(process.env.FIREBASE_CONFIG_PATH)
+
 if (!firebase.apps.length) {
-  const config = {
-    apiKey: "AIzaSyAt49eVAu3ZVWhYvktYDq8vGWFWsVBRiag",
-    authDomain: "video-share-d1a94.firebaseapp.com",
-    databaseURL: "https://video-share-d1a94.firebaseio.com",
-    projectId: "video-share-d1a94",
-    storageBucket: "video-share-d1a94.appspot.com",
-    messagingSenderId: "91171756450",
-    appId: "1:91171756450:web:67f0fa48a017da151a3afb",
-    measurementId: "G-MTQPDP7Z5J",
-  }
+  const config = firebaseConfig
   firebase.initializeApp(config)
 }
 
