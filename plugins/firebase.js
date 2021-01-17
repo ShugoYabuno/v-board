33994ae4f3b56f929a707087e458f6ded22b8028
ShugoYabuno <shugo.ybn@gmai.com>
2021-01-18 00:11:55 +0900

    update: firebase config 設定方法変更

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,10 +2,34 @@ import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/firestore"
 
-const firebaseConfig = require(process.env.FIREBASE_CONFIG_PATH)
+const firebaseConfig = () => {
+  if(process.env === "production") {
+    return {
+      apiKey: "AIzaSyAt49eVAu3ZVWhYvktYDq8vGWFWsVBRiag",
+      authDomain: "video-share-d1a94.firebaseapp.com",
+      databaseURL: "https://video-share-d1a94.firebaseio.com",
+      projectId: "video-share-d1a94",
+      storageBucket: "video-share-d1a94.appspot.com",
+      messagingSenderId: "91171756450",
+      appId: "1:91171756450:web:67f0fa48a017da151a3afb",
+      measurementId: "G-MTQPDP7Z5J"
+    }
+  } else {
+    return {
+      apiKey: "AIzaSyAt49eVAu3ZVWhYvktYDq8vGWFWsVBRiag",
+      authDomain: "video-share-d1a94.firebaseapp.com",
+      databaseURL: "https://video-share-d1a94.firebaseio.com",
+      projectId: "video-share-d1a94",
+      storageBucket: "video-share-d1a94.appspot.com",
+      messagingSenderId: "91171756450",
+      appId: "1:91171756450:web:67f0fa48a017da151a3afb",
+      measurementId: "G-MTQPDP7Z5J"
+    }
+  }
+}
 
 if (!firebase.apps.length) {
-  const config = firebaseConfig
+  const config = firebaseConfig()
   firebase.initializeApp(config)
 }
 
---
+++ plugins/firebaseConfig/v-board-pro.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyChKe7aXLONoyHOtQcOJonDYTvLj5HXiWw",
+  authDomain: "v-board-8cece.firebaseapp.com",
+  projectId: "v-board-8cece",
+  storageBucket: "v-board-8cece.appspot.com",
+  messagingSenderId: "442714615672",
+  appId: "1:442714615672:web:9a5fee9a0ce4d55e5be28d",
+  measurementId: "G-NVCWYL7MM5"
+}
