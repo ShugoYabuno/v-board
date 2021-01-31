aa751df4856d0e29036cc1cda5da5e13403f1984
ShugoYabuno <shugo.ybn@gmai.com>
2021-01-31 23:33:53 +0900

    update: シェア用URLをクリップボードにコピー

--- components/modules/layouts/sidebar/SidebarContent.vue
+++ components/modules/layouts/sidebar/SidebarContent.vue
@@ -3,6 +3,17 @@
     <div class="h-14 px-2">
       <TeamInfo />
     </div>
+    <button
+      v-clipboard:copy="shareLink"
+      v-clipboard:success="onCopy"
+      v-clipboard:error="onError"
+      class="flex-i-center w-full h-8 px-2 text-gray-80 hover:bg-primary-10">
+      <FontAwesomeIcon
+        icon="link" />
+      <p class="text-sm ml-1">
+        {{ shareMessage }}
+      </p>
+    </button>
     <nuxt-link
       :to="`/teams/${teamInfo.slug}/config`"
       class="flex-i-center h-8 px-2 text-gray-80 hover:bg-primary-10">
@@ -19,23 +30,53 @@
 import TeamInfo from "~/components/modules/TeamInfo"
 import VideoUploader from "~/components/parts/VideoUploader"
 import IconCog from "~/components/parts/fontawesome/Cog"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faLink } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import Vue from "vue"
+import VueClipboard from "vue-clipboard2"
+
+Vue.use(VueClipboard)
+
+library.add(faLink)
 
 export default {
   components: {
     VideoUploader,
     IconCog,
-    TeamInfo
+    TeamInfo,
+    FontAwesomeIcon
   },
   data() {
     return {
       teamInfo: {},
       profileIcon: "",
-      teamSlug: "/"
+      teamSlug: "/",
+      shareLink: "",
+      shareButtonMessage: "招待URLをコピー"
+    }
+  },
+  computed: {
+    shareMessage() {
+      return this.shareButtonMessage
     }
   },
   async mounted() {
-    this.teamInfo = this.$store.getters["teamInfo"]
+    this.shareLink = `https://v-board.app/sign_in?team=${this.$route.params.teamSlug}&openExternalBrowser=1`
+    this.teamInfo = await this.$store.getters["teamInfo"]
     if (this.teamInfo.slug) this.teamSlug = `/${this.teamInfo.slug}`
+  },
+  methods: {
+    onCopy(e) {
+      const defaultMessage = this.shareButtonMessage
+      this.shareButtonMessage = "コピーしました!"
+      setTimeout(() => {
+        this.shareButtonMessage = defaultMessage
+      }, 2000)
+    },
+    onError(e) {
+      alert("Failed to copy texts")
+    }
   }
 }
 </script>
