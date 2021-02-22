8d636191bc19ac736b5b28c874d53f4f09c0b36d
ShugoYabuno <shugo.ybn@gmai.com>
2021-02-22 19:55:59 +0900

    fix: サーバーサイドでstoreとれないので修正

--- middleware/initialize.js
+++ middleware/initialize.js
@@ -1,11 +1,11 @@
 export default async function ({ store, params }) {
+  if (params.teamSlug) {
+    await store.dispatch("setTeamInfoBySlug", {
+      teamSlug: params.teamSlug
+    })
+  }
   if (process.client) {
     await store.dispatch("initial")
-    if (params.teamSlug) {
-      await store.dispatch("setTeamInfoBySlug", {
-        teamSlug: params.teamSlug
-      })
-    }
     if (params.videoId) {
       await store.dispatch("isVideoShow", true)
     } else {
